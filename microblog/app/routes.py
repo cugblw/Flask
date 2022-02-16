e6cf117819c14fdfe9e5ac2a6e5f10e850ddc329
Weil Lee <cugblw204@outlook.com>
2022-02-16 08:57:00 +0800

    rename

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,7 +5,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Weil'}
     posts = [
         {
             'author': {'username': 'John'},
